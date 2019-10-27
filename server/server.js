0fbd3c53c3edddb726d14c57311af788c51aa393
eaudran <nevosis@gmail.com>
2019-10-27 23:27:15 +0100

    allow server to listen to / and res Hello World

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+app.get('/', function (req, res) {
+	console.log('Get /')
+  	res.send('Hello World!')
+	console.log('Sent "Hello World!"')
+})
+
 app.listen(port);
 
 console.log('C&B RESTful API server started on: ' + port);
