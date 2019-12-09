cbbf9abdb229ffd81867af4113432e227ffe62ea
eaudran <nevosis@gmail.com>
2019-12-09 23:54:26 +0100

    add Logger. Add message routes. Move message handle to correct directory

---
+++ server/api/routes/message.js
@@ -0,0 +1,25 @@
+const Messages = require("../models/messages");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function(req, res) {
+	Messages.find(function(err, messages) {
+		if (err) return console.error(err);
+		res.send(messages);
+	});
+});
+
+router.post("/", function(req, res) {
+	if (req.body && req.body.message) {
+		let message = new Messages({ message: req.body.message });
+		message.save(function(err, mess) {
+			if (err) res.status(500).send("Broken");
+			else res.send();
+		});
+	} else {
+		res.status(400).send("message mandatory")
+	}
+});
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,59 +1,58 @@
-var Messages = require("./api/models/messages");
-var cors = require("cors");
-var mongoose = require("mongoose");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-var express = require("express"),
+const express = require("express"),
 	app = express(),
 	port = process.env.PORT || 3000;
 
-app.use(cors());
+
+
+const messageRoutes = require("./api/routes/message");
+
+
+app.use(cors()); // Cross-origin resource sharing
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// Ces options sont recommandées par mLab pour une connexion à la base
-var options = {
+app.use(morgan("dev")); // DEBUGGER
+
+const options = {
 	server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
 	replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
 };
 
 //URL de notre base
-var mongoDB = "mongodb://127.0.0.1/cnb";
+const mongoDB = "mongodb://127.0.0.1/cnb";
 
 // Nous connectons l'API à notre base de données
 mongoose.connect(mongoDB, options);
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Erreur lors de la connexion"));
 db.once("open", function() {
 	console.log("Connexion à la base OK");
 });
 
-app.get("/", function(req, res) {
-	console.log("Get /");
 
-	res.send("Hello World!");
-	console.log('Sent "Hello World!"');
+
+// Routes which should handle requests
+app.use("/message", messageRoutes);
+
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.get("/message", function(req, res) {
-	console.log("Get /message");
-
-	Messages.find(function(err, messages) {
-		if (err) return console.error(err);
-		res.send(messages);
-	});
-});
-
-app.post("/message", function(req, res) {
-	if (req.body && req.body.message) {
-		var message = new Messages({ message: req.body.message });
-		message.save(function(err, mess) {
-			if (err) res.status(500).send("Broken");
-			else res.send();
-		});
-	} else {
-		res.status(400).send("message mandatory")
-	}
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 app.listen(port);
